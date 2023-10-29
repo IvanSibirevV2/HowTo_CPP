9ecec9b3cc124b444a51e643ea9d0bb2b3974b29
йцу <>
2023-10-29 12:24:37 +0300

    AutoCommit_29 октября 2023 г. 12:24:37_SibNout2023

--- Тверь_2017/000_CPPApp1/main.c
+++ Тверь_2017/000_CPPApp1/main.c
@@ -2,20 +2,26 @@
 #include <locale.h>
 int main(){
 	setlocale(LC_ALL, "Russian");
-	int a=7, b=2, res = 0;
-	res = a+b; printf("%d + %d = %d\n",a,b,res);
-	res = a-b; printf("%d - %d = %d\n",a,b,res);
-	res = a*b; printf("%d * %d = %d\n",a,b,res);
-	res = a/b; printf("%d / %d = %d\n",a,b,res);
-	res = a%b; printf("%d %% %d = %d\n",a,b,res);
+
+	int a=7, b;
+	float g= 9.81,v;
+	printf("a=%d; b=%d;\n",a,b);
+	printf("g=%f; v=%f;\n",g,v);
+	b = (int)g; //приводим значение 9.81 к типу int, получим 9
+	v= (float)a; // приводим значение 7 к типу float, получим 7.0
+	printf("После обертывания типа...\n");
+	printf("a=%d; b=%d;\n",a,b);
+	printf("g=%f; v=%f;\n",g,v);
+	
+	int res = 0;
 	res=getchar();
 	return 0;
 }
 /*
-7 + 2 = 9
-7 - 2 = 5
-7 * 2 = 14
-7 / 2 = 3
-7 % 2 = 1
+a=7; b=4199400;
+g=9,810000; v=0,000000;
+После обертывания типа...
+a=7; b=9;
+g=9,810000; v=7,000000;
 Что то такм про нажатие клавиши....
 */
