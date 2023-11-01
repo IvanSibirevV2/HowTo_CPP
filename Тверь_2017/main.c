f412a3c4a22db72a5465251dc3a8f21745484b9e
йцу <>
2023-11-02 00:26:23 +0300

    AutoCommit_2 ноября 2023 г. 0:26:23_SibNout2023

--- Тверь_2017/main.c
+++ Тверь_2017/main.c
@@ -1,8 +1,14 @@
+//Приведение типов
 #include <stdio.h>
-int main(int argc, char *argv[])
-{
-	printf("Hello C-Free!\n");
-	int a = 2;
-	printf("a = %d\n", a);
+#include <locale.h>
+int main(){
+	setlocale(LC_ALL, "Russian");
+	int a=7, b;float g= 9.81,v;
+	printf("a=%d; b=%d;\n",a,b);printf("g=%f; v=%f;\n",g,v);
+	b = (int)g; //приводим значение 9.81 к типу int, получим 9
+	v= (float)a; // приводим значение 7 к типу float, получим 7.0
+	printf("После обертывания типа...\n");
+	printf("a=%d; b=%d;\n",a,b);printf("g=%f; v=%f;\n",g,v);
+	//int res = 0;res=getchar();
 	return 0;
 }
