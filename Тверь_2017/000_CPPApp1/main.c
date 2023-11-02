a7bf191c991206d0b4d4b3c8165895de3bf845b3
йцу <>
2023-11-02 14:32:27 +0300

    AutoCommit_$((Get-Date).DateTime)_$($Env:UserName)

--- Тверь_2017/000_CPPApp1/main.c
+++ Тверь_2017/000_CPPApp1/main.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 int main(){
 	setlocale(LC_ALL, "Russian");
 
-	int a=7, b;
-	float g= 9.81,v;
-	printf("a=%d; b=%d;\n",a,b);
-	printf("g=%f; v=%f;\n",g,v);
-	b = (int)g; //приводим значение 9.81 к типу int, получим 9
-	v= (float)a; // приводим значение 7 к типу float, получим 7.0
-	printf("После обертывания типа...\n");
-	printf("a=%d; b=%d;\n",a,b);
-	printf("g=%f; v=%f;\n",g,v);
-	
-	int res = 0;
-	res=getchar();
+	int a=7;
+	printf("a=%d;\n",a);
+	int res = 0;res=getchar();
 	return 0;
 }
 /*
--- Тверь_2017/main.c
+++ Тверь_2017/main.c
@@ -1,11 +1,18 @@
+//Приведение типов
 #include <stdio.h>
-int main(int argc, char *argv[])
-{
-	printf("Hello C-Free!\n");
-	int age=0;
-	scanf("%d", &age);
-	printf("age = %d\n", age);
-	int res = 0;res=getchar();
-    printf("res = <%c>\n", res);
+#include <locale.h>
+#include <math.h>
+int main(){
+	setlocale(LC_ALL, "Russian");float a=0;
+	a=-1.4;printf("fabs(%lg)=%lg;\n",a,fabs(a));
+	a=4;printf("sqrt(%lg)=%lg;\n",a,sqrt(a));
+	a=M_PI;printf("M_PI=%lg;\n",a);
+	a=M_PI;printf("sin(%lg)=%lg; (В радианах)\n",a,sin(a));
+	a=M_PI;printf("cos(%lg)=%lg; (В радианах)\n",a,cos(a));
+	a=4;printf("pow(%lg, 2)=%lg; (4^2)\n",a,pow(a, 2));
+	a=1;printf("exp(%lg)=%lg;\n",a,exp(a));
+	a=4;printf("log(%lg)=%lg; (натуральный логарифм числа по основанию экспоненты)\n",a,log(a));
+	a=4;printf("log10(%lg)=%lg; (десятичный логарифм числа по основанию экспоненты)\n",a,log10(a));
+    int res = 0;res=getchar();
 	return 0;
 }
