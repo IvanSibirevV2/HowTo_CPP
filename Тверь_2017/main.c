6349f6cc715fe846a8f13bcce2c1e3e195038f6c
йцу <>
2023-11-10 14:38:22 +0300

    AutoCommit_10 ноября 2023 г. 14:38:21_SibNout2023

--- Тверь_2017/main.c
+++ Тверь_2017/main.c
@@ -1,18 +1,10 @@
-//Приведение типов
 #include <stdio.h>
-#include <locale.h>
-#include <math.h>
-int main(){
-	setlocale(LC_ALL, "Russian");float a=0;
-	a=-1.4;printf("fabs(%lg)=%lg;\n",a,fabs(a));
-	a=4;printf("sqrt(%lg)=%lg;\n",a,sqrt(a));
-	a=M_PI;printf("M_PI=%lg;\n",a);
-	a=M_PI;printf("sin(%lg)=%lg; (В радианах)\n",a,sin(a));
-	a=M_PI;printf("cos(%lg)=%lg; (В радианах)\n",a,cos(a));
-	a=4;printf("pow(%lg, 2)=%lg; (4^2)\n",a,pow(a, 2));
-	a=1;printf("exp(%lg)=%lg;\n",a,exp(a));
-	a=4;printf("log(%lg)=%lg; (натуральный логарифм числа по основанию экспоненты)\n",a,log(a));
-	a=4;printf("log10(%lg)=%lg; (десятичный логарифм числа по основанию экспоненты)\n",a,log10(a));
-    int res = 0;res=getchar();
-	return 0;
+//Шаблонизация макросом
+#define SUM(x,y) (a##x + a##y)
+int main()
+{
+  int a1 = 5, a2 = 3;
+  printf("%d", SUM(1, 2)); // (a1 + a2)
+  getchar();
+  return 0;
 }
