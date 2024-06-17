5a9c2210c478f1d968e4e8abb76c9d10ff6bd998
йцу <>
2024-06-17 10:17:58 +0300

    AutoCommit_17 июня 2024 г. 10:17:58_SibNout2023

--- Тверь_2017/main.c
+++ Тверь_2017/main.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define P 2
-int main()
+int main(int argc, char *argv[])
 {
-  system("chcp 1251");
-  system("cls");
-#if P==1
-  printf("Выполняется ветка 1");
-#elif P==2
-  printf("Выполняется ветка 2, P=%d", P);
-#else
-  printf("Выполняется другая ветка, P=%d", P);
-#endif
-  getchar();
-  return 0;
+	printf("Hello C-Free!\n");
+	int a = 2;
+	printf("a = %d\n", a);
+	return 0;
 }
