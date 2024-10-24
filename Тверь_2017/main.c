e978b4910868b5573b184010df92bd6f6eb53ec9
йцу <>
2024-10-24 13:19:50 +0300

    AutoCommit_24 октября 2024 г. 13:19:50_SibNout2023

--- Тверь_2017/main.c
+++ Тверь_2017/main.c
@@ -2,7 +2,10 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello C-Free!\n");
-	int a = 2;
-	printf("a = %d\n", a);
+	int age=0;
+	scanf("%d\n", &age);
+	printf("age = %d\n", age);
+	int res = 0;res=getchar();
+    printf("res = <%c>\n", res);
 	return 0;
 }
